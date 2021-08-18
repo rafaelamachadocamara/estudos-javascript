4b3c6e2c713b90e5bbe5c0d623b88f8a8cdaf1ad
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-08-18 15:46:31 -0300

    adicionando comentarios

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //para mostrar um alerta na página
 alert('Hello World!');
+
+//os comentarios são feitos utilizando duas barras, como por exemplo:
+//alert("Olá mundo!")
+
+//comentários de multiplas linhas são feitas utilizando asterisco e barras, como por exemplo:
+/*oi 
+galera*/
