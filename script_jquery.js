81ab2c09047e749efac9eec3730f7c10f4eabaca
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-08-19 16:18:58 -0300

    alterando css atraves do jquery

--- script_jquery.js
+++ script_jquery.js
@@ -3,3 +3,21 @@ $(function(){
         $('h1').hide();
     });
 })
+
+$(function(){
+        $('h1').css("color", "red");
+});
+
+$(function(){
+    $('#unico').css("color", "green");
+});
+
+$(function(){
+    $('#azul').click(function(){
+        $('p').css("background-color", "blue");
+    });
+
+    $('#vermelho').click(function(){
+        $('p').css("background-color", "red");
+    });
+});
