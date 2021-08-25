a1cfaf85a20f1eed95ed986701ed90f76911f5f5
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-08-25 15:33:14 -0300

    strings

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -21,3 +21,34 @@ console.log("Olá Mundo 2");
     //Para saber quais são os valores máximos e minimos
     console.log(Number.MAX_VALUE);
     console.log(Number.MIN_VALUE);
+
+    /* Tipo de dados 
+    Strings: */
+
+    //São valores de texto, normalmente chamados de "cadeias de caracteres"
+    let = "Olá Mundo";
+
+    //Para adicionar caracteres especiais
+    let caracteresEspeciais = "Lorem \n Ipsum \\ dolar \' \" amet";
+
+    /* 
+    \n = Nova linha 
+    \\: exibe a \
+    \': Exibe '
+    \" Exibe "
+    */
+
+    //Concatenação de Strings 
+    //Utilizando o sinal de +
+    let ola = "Olá";
+    let mundo = "Mundo";
+
+    let olaMundo = ola+mundo;
+
+    //Utilizando o String.concat
+    let olaMundo = ola.concat(mundo);
+    //Este método tambem permite concatenar diversas Strings
+    let olaMundo = ola.concat(mundo,"frase",ola);
+
+    //Template de Strings
+    let olaMundo = `${ola} ${mundo} ${1+1}`;
