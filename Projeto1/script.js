8ac3bd8106ff098452b09c8e55c84b66f9c949af
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-08-25 15:24:51 -0300

    numbers

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -1,1 +1,23 @@
 console.log("Olá Mundo 2");
+
+/* Tipo de dados 
+    Numbers: */
+
+//Podem ser numeros inteiros, numeros decimais, numeros negativos inteiros e negativos decimais
+    let numeroInteiro =100;
+    let numeroDecimal = 5.38;
+    let numeroNegativo = 200.99;
+
+    //Operadores matemáticos
+    let numero = 5;
+    let numero2 = 7;
+    let resultado = numero + numero2;
+
+    let resultado = numero * 0.5;
+
+    //Cuidado, divisão por 0 pode causar erros
+    let resultado = numero /0;
+
+    //Para saber quais são os valores máximos e minimos
+    console.log(Number.MAX_VALUE);
+    console.log(Number.MIN_VALUE);
