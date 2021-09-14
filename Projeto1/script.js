c26f4b78c8b77a799627e69a72c5eb57d7ac39b2
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-09-14 11:38:09 -0300

    lacos de repeticao: break e continue

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -486,3 +486,39 @@ do{
 while(num < 10);
 
 //Ambos "while" e "do while" tem uma lógica similar. A situação definirá qual o melhor loop para seu código
+
+/* Tipos de laços de repetição:
+     
+            Comandos que podemos utilizar: break */
+
+//O break é um comando que podemos utilizar quando queremos parar/encerrar o loop na hora
+
+let num = 1;
+
+while(num < 10){
+    console.log(num);
+    if(num == 6){
+        break;
+    }
+    num++;
+}
+
+for(let numero of [1,2,3,4,5,6,7]){
+    console.log(numero);
+    if(numero == 5){
+        break;
+    }
+}
+
+/* Tipos de laços de repetição:
+     
+            Comandos que podemos utilizar: continue */
+
+//Com esse comabndo. é possivel iniciar a proxima repetição do loop
+
+for(let numero of [1,2,3,4,5,6,7]){
+    if(numero == 5 || numero == 7){
+        continue;
+    }
+    console.log(numero);
+}
